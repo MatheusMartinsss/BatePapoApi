874e53176c502feab444f47ded7f21417ff6c9bf
MatheusMartinsss <matheus.s.martins98@gmail.com>
2021-09-20 12:17:44 -0400

    teste

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const server = express().listen(port, () => console.log('Listening on ${port}'))
 
 const io = require("socket.io")(server, {
     cors: {
-        origin: "*",
+        origin: "http://localhost:3000/",
     },
 });
 
